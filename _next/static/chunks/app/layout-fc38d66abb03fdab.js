8068f176238591920ef2d38d538d4b2d98fee3c6
Ken Chu <chuwingyu@gmail.com>
2024-05-28 17:50:54 +0800

    Deploy

---
+++ _next/static/chunks/app/layout-fc38d66abb03fdab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1091:function(n,e,_){Promise.resolve().then(_.t.bind(_,3247,23))},3247:function(n){n.exports={style:{fontFamily:"'__Inter_3b8883', '__Inter_Fallback_3b8883'",fontStyle:"normal"},className:"__className_3b8883"}}},function(n){n.O(0,[972,971,23,744],function(){return n(n.s=1091)}),_N_E=n.O()}]);
